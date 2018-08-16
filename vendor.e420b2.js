26ab9d18be16825994274418350b9f41651677b0
nikdementev <nikita.pietrov.71@mail.ru>
2018-08-16 10:31:11 +0300

    update

--- vendor.e420b2.js
+++ vendor.e420b2.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + "82fc8d" + ".chunk.js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + "e420b2" + ".chunk.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
